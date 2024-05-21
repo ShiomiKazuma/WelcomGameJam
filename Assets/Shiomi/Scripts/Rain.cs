eb9a3b0bcdf2ae9f4b36efa32090d53f29831033
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2024-05-21 13:23:56 +0900

    [fix]雨の処理の修正

--- Assets/Shiomi/Scripts/Rain.cs
+++ Assets/Shiomi/Scripts/Rain.cs
@@ -7,17 +7,24 @@ public class Rain : MonoBehaviour
     [SerializeField] float _focePower = 100f;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        GameObject player = collision.gameObject;
-        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
-        SpriteRenderer sprite = player.GetComponent<SpriteRenderer>();
-        // ノックバック処理
-        if(!sprite.flipX)
+        if(collision.gameObject.tag == "Player")
         {
-            rb.AddForce(transform.right * _focePower);
+            GameObject player = collision.gameObject;
+            Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+            SpriteRenderer sprite = player.GetComponent<SpriteRenderer>();
+            // ノックバック処理
+            if (!sprite.flipX)
+            {
+                rb.AddForce(transform.right * _focePower);
+            }
+            else
+            {
+                rb.AddForce(-transform.right * _focePower);
+            }
         }
         else
         {
-            rb.AddForce(-transform.right * _focePower);
+            Destroy(collision.gameObject);
         }
     }
 }
