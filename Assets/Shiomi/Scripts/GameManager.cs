732ec2f2609623b1a7628a290ca512ac8f0f33db
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2024-05-21 14:14:45 +0900

    [add]リスポーン処理を追加

---
+++ Assets/Shiomi/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : SingletonBase<GameManager>
+{
+    float _time;
+    bool IsStart = false;
+    protected override void DoAwake()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(IsStart)
+        {
+            _time += Time.deltaTime;
+        }
+    }
+
+    public void Start()
+    {
+        IsStart = true;
+    }
+    public void Goal()
+    {
+        IsStart = false;
+    }
+
+
+}
---
+++ Assets/Shiomi/Scripts/ResponManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResponManager : MonoBehaviour
+{
+    [SerializeField] List<Transform> _respon = new List<Transform>();
+    int _responNum = 0;
+
+    public void ResponChange()
+    {
+        _responNum++;
+    }
+
+    public Transform Respon()
+    {
+        return _respon[_responNum];
+    }
+}
